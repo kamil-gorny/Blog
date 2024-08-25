f25df3896664ee4a77225a99eade4634a2414d85
Kamil Górny <kamil.gorny99@gmail.com>
2024-08-25 18:35:09 +0200

    feat: Add header to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,39 @@
 <script>
-	import digital from '$lib/images/digital.webp';
+	import mainImage from '$lib/images/mainimage.jpg';
 </script>
 
 
 <!-- <img src="{digital}" alt=""/> -->
+
+<main>
+	<div class="header-container">
+		<span class="header-main">Hi, I am Kamil.</span>
+		<span class="header-sub">Web developer and cybersec enthusiast.</span>	
+	</div>	
+	<img class="hero" src="{mainImage}" alt="">
+</main>
+
+<style>
+	.hero{
+		width: 981px;
+		height: 528px;
+	}
+	main{
+		text-align: center;
+	}
+	.header-main{
+		font-size: 53px;
+		font-weight: 600;
+	}
+	.header-sub{
+		font-size: 28px;
+		font-family: "Inter", sans-serif;
+	}
+	.header-container{
+		padding-top:72px;
+		padding-bottom: 81px;
+		display: flex;
+		flex-direction: column;
+		gap:12px;
+	}
+</style>
