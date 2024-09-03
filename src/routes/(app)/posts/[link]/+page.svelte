3b46207b348e391257c2f7f418f4435f27fe9e63
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-03 07:00:53 +0200

    style: Style post header

--- src/routes/(app)/posts/[link]/+page.svelte
+++ src/routes/(app)/posts/[link]/+page.svelte
@@ -1,6 +1,56 @@
 <script>
+    import {FaFacebook, FaTwitter, FaLink} from "svelte-icons/fa";
+    import { marked } from 'marked';
     export let data;
 </script>
+<div class="container">
 <div class="post-header">
-    {data.post.title}
+    <p class="post-header-date">{data.post.date}</p>
+    <h1>{data.post.title}</h1>
+    <div class="socials">
+        <div class="icon">
+            <FaFacebook/>
+        </div>
+        <div class="icon">
+            <FaTwitter/>
+        </div>
+        <div class="icon">
+            <FaLink />
+        </div>
+
+
+    </div>
 </div>
+</div>
+
+<style>
+    .post-header-date{
+        color:#7A7A7E;
+    }
+    .icon{
+        color:#7E7E7E;
+    }
+    .container{
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .socials{
+        padding-top: 11px;
+        display: flex;
+        height: 16px;
+        justify-content: start;
+        gap: 8px;
+    }
+    .post-header{
+        padding-top: 100px;
+        width:80%;
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+    }
+    h1{
+        padding: 0;
+        margin: 0;
+    }
+</style>
