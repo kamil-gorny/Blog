3f12db0e966f189a1223e14dca7bc3dd5b290217
Kamil Górny <kamil.gorny99@gmail.com>
2024-09-07 10:12:35 +0200

    feat: Add technology button component

---
+++ src/TechnologyButton.svelte
@@ -0,0 +1,24 @@
+<script>
+    import Icon from '@iconify/svelte';
+    export let icon;
+    export let name;
+</script>
+
+<div class="test">
+    <Icon {icon}/>
+    <p>{name}</p>
+</div>
+
+<style>
+    .test{
+        display: inline-flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        background-color: #eee;
+        border-radius: 5px;
+
+
+
+    }
+</style>
---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+import TechnologyButton  from "../../TechnologyButton.svelte";
+
+</script>
+
+<div class="container">
+    <span class="header">Kamil Górny</span>
+   <p>I’m a software engineer specializing in .NET and C#.</p>
+    <div>Things I work with:   <TechnologyButton name="Linkedin" icon="akar-icons:linkedin-fill"/></div>
+<p>
+       Outside of programming I have a strong passion for cybersecurity and actively participate in bug bounty programs during my free time. My focus is on building secure and scalable systems that empower developers to create with confidence.
+
+       I’m currently based in Gdańsk, Poland.
+
+   </p>
+    <p>Find me on</p>
+
+
+    Or mail me at contact@kamilgorny.dev
+</div>
+
+
+<style>
+    .header{
+        font-size: 32px;
+        font-weight: 700;
+    }
+    .container{
+        padding-top: 50px;
+        color: #1D1D1F;
+        justify-content: center;
+        align-self: center;
+        flex-direction: column;
+        display: flex;
+        gap:10px;
+        width: 50%;
+    }
+
+</style>
