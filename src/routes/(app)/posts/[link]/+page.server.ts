620f1fc77af02fe1a215c7b926df57b20a4f7f44
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-08 23:02:50 +0200

    style: Style code blocks

--- src/routes/(app)/posts/[link]/+page.server.ts
+++ src/routes/(app)/posts/[link]/+page.server.ts
@@ -1,14 +1,31 @@
 import { posts } from '../data.js';
-import { compile } from "mdsvex";
+import {escapeSvelte, mdsvex, compile} from 'mdsvex'
 import {error, Load} from "@sveltejs/kit";
+import {createHighlighter} from "shiki";
 
 export const load: Load = async ({ params }) => {
     const post = posts.find(post => post.link === params.link);
     if (!post) throw error(404);
-    const compiledResponse = await compile(post.content);
+
+    const mdsvexOptions = {
+        extensions: ['.md'],
+        highlight: {
+            highlighter: async (code, lang = 'text') => {
+                const highlighter = await createHighlighter({
+                    themes: ['github-dark'],
+                    langs: ['javascript', 'typescript','bash'],
+                })
+                await highlighter.loadLanguage('javascript', 'typescript', 'bash')
+                const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'github-dark' }))
+                return `${html}`
+            }
+
+        },
+
+    }
+    const compiledResponse = await compile(post.content, mdsvexOptions);
 
     return {
-        content: compiledResponse?.code.replace(/>{@html `<code class="language-/g, '><code class="language-')
-            .replace(/<\/code>`}<\/pre>/g, '</code></pre>')
+        content: compiledResponse?.code
     };
 };
