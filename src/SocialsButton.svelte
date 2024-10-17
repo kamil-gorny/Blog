4d4759bae475a8ea613f70d07d4a2b4850f07dc5
Kamil Górny <kamil.gorny99@gmail.com>
2024-10-17 18:03:54 +0200

    refactor: Rewrite socials button to tailwind

--- src/SocialsButton.svelte
+++ src/SocialsButton.svelte
@@ -5,17 +5,13 @@
     export let href;
 </script>
 
-<a href="{href}" class="inline-flex justify-center items-center border-b-gray-200 gap-1 text-lg border-b border-gray-200 hover:border-gray-600">
+<a href="{href}" class=" hover:border-b-gray-400 hover:cursor-pointer inline-flex justify-center items-center border-b-gray-200 gap-1 text-lg border-b border-gray-200 hover:border-gray-600">
     <Icon {icon} />
     <span class="name">{name}</span>
 </a>
 
 <style>
 
-    .container:hover{
-        border-bottom: 1px solid #1D1D1F;
-        cursor: pointer;
-    }
     a{
         text-decoration: none;
         color: #1D1D1F;
