4bd997bcf529e51d7b0e4efc681c115c61dd376a
Kamil Górny <kamil.gorny99@gmail.com>
2024-12-23 16:38:48 +0100

    feat: Add experience timeline

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -2,8 +2,10 @@
 import TechnologyButton  from "../../TechnologyButton.svelte";
 import profilePicture from '$lib/images/profile-image.jpg';
 import { fade } from 'svelte/transition';
+import Icon from '@iconify/svelte';
 import {onMount} from "svelte";
 import SocialsButton from "../../SocialsButton.svelte";
+import { Timeline, TimelineItem, Activity, ActivityItem, Group, GroupItem } from 'flowbite-svelte';
 let isVisible = false;
 
 
@@ -15,7 +17,10 @@ onMount(() =>{
 <div class="pt-12 flex flex-col justify-center w-1/2 self-center" in:fade>
 
     <span class="text-3xl font-bold">Kamil Górny</span>
+    <p class="flex flex-row items-center"><Icon icon="mdi:location"></Icon>Gdańsk, Poland</p>
+    <br>
    <p>I’m a software engineer specializing in .NET and C#.</p>
+ 
     <div class="flex flex-row items-center gap-56">
     <div class="technologies">
         <div>Things I work with:
@@ -33,7 +38,23 @@ onMount(() =>{
     </div>
     <img  src="{profilePicture}" alt="me" class="w-48 h-48 rounded-xl -rotate-3 border-blue-500 border-4 "/>
     </div>
-
+    <script>
+        import { Timeline, TimelineItem, Button } from 'flowbite-svelte';
+        import { ArrowRightOutline } from 'flowbite-svelte-icons';
+      </script>
+      <h1>Experience</h1>
+      <Timeline>
+        <TimelineItem title=" .Net Tech Lead" date="February 2022 - Present">
+          <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Nsure.com</p>
+          <!-- <Button color="alternative">Learn more<ArrowRightOutline class="ms-2 w-5 h-5" /></Button> -->
+        </TimelineItem>
+        <TimelineItem title=".Net Developer" date="January 2021 - Present">
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">Nsure.com</p>
+        </TimelineItem>
+        <TimelineItem title="Junior .Net Developer" date="April 2022">
+          <p class="text-base font-normal text-gray-500 dark:text-gray-400">Flexible IT</p>
+        </TimelineItem>
+      </Timeline>
 <p class="pt-10">
        Outside of programming I have a strong passion for cybersecurity and actively participate in bug bounty programs during my free time.
     <br><br>My focus is on building secure and scalable systems that empower developers to create with confidence.
